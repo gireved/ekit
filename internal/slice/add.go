c042658dd521fdfd585f5fae6aee24bc351fa98c
wuhaoxin <125979289+gireved@users.noreply.github.com>
2024-12-11 12:43:50 +0800

    slice的Add方法

---
+++ internal/slice/add.go
@@ -0,0 +1,21 @@
+package slice
+
+import "my-ekit/internal/errs"
+
+func Add[T any](src []T, element T, index int) ([]T, error) {
+	length := len(src)
+	if index < 0 || index > length {
+		return nil, errs.NewErrIndexOutOfRange(length, index)
+	}
+
+	// 先将src扩展一个元素
+	var ZeroValue T
+	src = append(src, ZeroValue)
+	for i := len(src) - 1; i > index; i-- {
+		if i-1 >= 0 {
+			src[i] = src[i-1]
+		}
+	}
+	src[index] = element
+	return src, nil
+}
